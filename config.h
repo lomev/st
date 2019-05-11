27f74c8f1328c472ab66131344feac010504fb8e
reiffarth <ravd@tuta.io>
2019-05-11 09:50:08 +0200

    Change colorscheme to solarized

--- config.h
+++ config.h
@@ -84,41 +84,32 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-    /* 8 normal colors */
-    "#050505",
-    "#8c4665",
-    "#287373",
-    "#7c7c99",
-    "#395573",
-    "#5e468c",
-    "#31658c",
-    "#eeeeee",
-
-    /* 8 bright colors */
-    "#3d3d3d",
-    "#bf4d80",
-    "#53a6a6",
-    "#9e9ecb",
-    "#477ab3",
-    "#7e62b3",
-    "#6096bf",
-    "#c0c0c0",
-
-    [255] = 0,
-
-    /* more colors can be added after 255 to use with DefaultXX */
-    "#cccccc",
-    "#555555",
+    "#073642",  /*  0: black    */
+    "#dc322f",  /*  1: red      */
+    "#859900",  /*  2: green    */
+    "#b58900",  /*  3: yellow   */
+    "#268bd2",  /*  4: blue     */
+    "#d33682",  /*  5: magenta  */
+    "#2aa198",  /*  6: cyan     */
+    "#eee8d5",  /*  7: white    */
+    "#002b36",  /*  8: brblack  */
+    "#cb4b16",  /*  9: brred    */
+    "#586e75",  /* 10: brgreen  */
+    "#657b83",  /* 11: bryellow */
+    "#839496",  /* 12: brblue   */
+    "#6c71c4",  /* 13: brmagenta*/
+    "#93a1a1",  /* 14: brcyan   */
+    "#fdf6e3",  /* 15: brwhite  */
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 12;
+unsigned int defaultbg = 8;
+static unsigned int defaultcs = 14;
+static unsigned int defaultrcs = 15;
 
 /*
  * Default shape of cursor
